cf0251229f71377dd88566996f097754722b1685
Bryan Chicaiza <chicaizabryan1dmat@hotmail.com>
2023-06-28 21:26:42 -0500

    conexion a ws

---
+++ petDiagnostic/Datos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace petDiagnostic
+{
+    public class Datos
+    {
+        private int codigo;
+        private string nombre;
+        private string apellido;
+        private string edad;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Edad { get => edad; set => edad = value; }
+    }
+}
--- petDiagnostic/MainPage.xaml.cs
+++ petDiagnostic/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,9 +13,24 @@ namespace petDiagnostic
 {
     public partial class MainPage : ContentPage
     {
+        private const string url = "http://192.168.56.1/ws_uisrael/moviles/post.php";
+        private HttpClient client = new HttpClient();
+        private ObservableCollection<petDiagnostic.Datos> _post;
+
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(url);
+            List<petDiagnostic.Datos> listPost = JsonConvert.DeserializeObject<List<petDiagnostic.Datos>>(content);
+            _post = new ObservableCollection<petDiagnostic.Datos>(listPost);
+
+            MyListView.ItemsSource = _post;
+
         }
     }
 }
