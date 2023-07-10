5c960ad8cd49ee21a359459eb85764e1db269d93
Mayra Lafebre <mayralafebre@gmail.com>
2023-07-10 11:15:25 -0500

    Pantallas Login, Mis Mascotas, Mascota

---
+++ petDiagnostic/Mascota.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace petDiagnostic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Mascota : ContentPage
+    {
+        public Mascota()
+        {
+            InitializeComponent();
+            List<Consulta> items = new List<Consulta>
+            {
+                new Consulta { Fecha = "2021-04-04", Texto = "Dolor Abdominal" },
+                new Consulta { Fecha = "2022-05-23", Texto = "Dolor Abdominal" },
+                new Consulta { Fecha = "2023-01-01", Texto = "Dolor Abdominal" }
+            };
+
+            myListView.ItemsSource = items;
+        }
+    }
+}
+
+public class Consulta
+{
+    public string Fecha { get; set; }
+    public string Texto { get; set; }
+}
---
+++ petDiagnostic/MisMascotas.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Android.Content.Res;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace petDiagnostic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MisMascotas : ContentPage
+    {
+        public MisMascotas()
+        {
+            InitializeComponent(); 
+            List<Item> items = new List<Item>
+            {
+                new Item { Imagen = "huella.png", Texto = "Elemento 1" },
+                new Item { Imagen = "huella.png", Texto = "Elemento 1" },
+                new Item { Imagen = "huella.png", Texto = "Elemento 1" }
+            };
+
+            myListView.ItemsSource = items;
+        }
+
+        private async void myListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await Navigation.PushAsync(new Mascota());
+        }
+    }
+}
+
+public class Item
+{
+    public string Imagen { get; set; }
+    public string Texto { get; set; }
+}
