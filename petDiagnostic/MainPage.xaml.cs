39096a2dacf26a7961fa57fb096c16aadbc7d4fd
[bryanedgar20] <[chicaizabryan1dmat@hotmail.com]>
2023-07-16 14:26:07 -0500

    nueva pantalla para registro de usuarios

--- petDiagnostic/MainPage.xaml.cs
+++ petDiagnostic/MainPage.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Android.Database;
+using Java.Util;
+using Newtonsoft.Json;
+using petDiagnostic.ObjetosVO;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,6 +16,10 @@ namespace petDiagnostic
 {
     public partial class MainPage : ContentPage
     {
+        
+        private HttpClient client = new HttpClient();
+        //private ObservableCollection<petDiagnostic.Datos> _post;
+        //private Usuario _posts;
 
         public MainPage()
         {
@@ -18,7 +28,25 @@ namespace petDiagnostic
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new MisMascotas());
+            string url = $"http://192.168.56.1:8081/usuario/obtenerUsuarioLogin/{txtUsuario.Text}/{txtClave.Text}";
+            var content = await client.GetStringAsync(url);
+            Usuario usuario = JsonConvert.DeserializeObject<Usuario>(content);
+          
+
+            if (null != usuario)
+            {
+
+                await Navigation.PushAsync(new MisMascotas());
+            }
+            else
+            {
+                await DisplayAlert("Error", "Credenciales Incorrectas", "Ok");
+            }
+        }
+
+        private void btbRegistrar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MntUsuario());
         }
     }
 }
---
+++ petDiagnostic/MntUsuario.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Java.Util;
+using Newtonsoft.Json;
+using petDiagnostic.ObjetosVO;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace petDiagnostic
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MntUsuario : ContentPage
+    {
+        public MntUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void btbGuardar_Clicked(object sender, EventArgs e)
+        {
+            var errors = this.validarCamposrequeridos();
+
+            if (null != errors && errors.Count == 0)
+            {
+                WebClient client = new WebClient();
+                string url = "http://192.168.56.1:8081/usuario/crearActualizarUsuario";
+
+                Usuario usuario = new Usuario();
+                usuario.PrimerNombre = txtPrimerNombre.Text.Trim();
+                usuario.PrimerApellido = txtPrimerApellido.Text.Trim();
+                usuario.SegundoNombre = txtSegundoNombre.Text;
+                usuario.SegundoApellido = txtSegundoApellido.Text;
+
+                //usuario.FechaNacimiento = (Date)Date.Parse(dateFechaNacimiento.Date.ToString("dd/MM/yyyy"));
+                usuario.Identificacion = txtIdentificacion.Text.Trim();
+                usuario.Email = txtEmail.Text.Trim();
+                usuario.NumeroTelefono = txtNroContacto.Text.Trim();
+                usuario.CallePrincipal = txtCallePrincipal.Text.Trim();
+                usuario.CalleSecundaria = txtCalleSecundaria.Text;
+                usuario.NickName = txtNickName.Text.Trim();
+                usuario.Clave = txtClave.Text.Trim();
+
+
+                string jsonData = JsonConvert.SerializeObject(usuario);
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+
+                byte[] response = client.UploadData(url, "POST", Encoding.UTF8.GetBytes(jsonData));
+                string responseString = Encoding.UTF8.GetString(response);
+
+                DisplayAlert("Exito", "Usuario creado exitosamente", "Aceptar");
+
+            }
+            else
+            {
+                var mensajeError = "Los siguientes campos son requeridos: "+ String.Join(",\n", errors);
+                DisplayAlert("Error",mensajeError, "Ok");
+            }
+
+        }
+
+        private List<String> validarCamposrequeridos()
+        {
+            List<String> errores = new List<string>();
+
+            if (String.IsNullOrWhiteSpace(txtPrimerNombre.Text))
+            {
+                errores.Add("Primer nombre");
+            }
+            if (String.IsNullOrWhiteSpace(txtPrimerApellido.Text))
+            {
+                errores.Add("Primer apellido");
+            }
+            if (String.IsNullOrWhiteSpace(dateFechaNacimiento.Date.ToString()))
+            {
+                errores.Add("Fecha nacimiento");
+            }
+            if (String.IsNullOrWhiteSpace(txtIdentificacion.Text))
+            {
+                errores.Add("Nro identificacion");
+            }
+            if (String.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                errores.Add("Correo electronico");
+            }
+            if (String.IsNullOrWhiteSpace(txtNroContacto.Text))
+            {
+                errores.Add("Nro contacto");
+            }
+            if (String.IsNullOrWhiteSpace(txtCallePrincipal.Text))
+            {
+                errores.Add("Calle principal");
+            }
+            if (String.IsNullOrWhiteSpace(txtNickName.Text))
+            {
+                errores.Add("Usuario");
+            }
+
+            if (String.IsNullOrWhiteSpace(txtClave.Text))
+            {
+                errores.Add("Clave");
+            }
+
+            if (String.IsNullOrWhiteSpace(txtPrimerNombre.Text) &&
+                txtClave.Text != txtClaveConfirmar.Text)
+            {
+                errores.Add("Las claves deben coincidir");
+            }
+            return errores;
+        }
+    }
+}
